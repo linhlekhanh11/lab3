b29a47142dcd30a277229ffd559fb41da5375cd6
Linh Le <lkl007@ucsd.edu>
2021-01-20 23:33:21 -0800

    update js file

--- hello/app.js
+++ hello/app.js
@@ -8,9 +8,10 @@ var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars')
 
-var hello = require('./routes/hello');
-
 var app = express();
+var hello = require('./routes/hello');
+app.get('/hello/:userName', hello.view);
+
 
 // all environments
 app.set('port', process.env.PORT || 3000);
